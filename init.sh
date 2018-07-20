67a31ac36544b0730b08f76adc17cef54e7fb61c
李通 <ghostong@126.com>
2018-07-20 16:00:12 +0800

    添加bat

--- init.sh
+++ init.sh
@@ -30,7 +30,10 @@ $PaMa install -y python-pip pkg-config
 $PaMa install -y htop iotop vim unzip tree
 
 #pip 安装
-pip install speedometer
+Speedometer=$(type -p speedometer)
+if [ ! "$Bat" ]; then
+    pip install speedometer
+fi
 echo "alias sm='speedometer'" >> $BashAuto
 
 #progress
@@ -40,12 +43,15 @@ if [ ! "$Progress" ]; then
     $PaMa install -y ncurses-devel
     unzip -o $SoftDir'/progress.zip' -d $TmpDir
     cd $TmpDir'/progress' && make && make install
-    echo "alias progress='progress -M'" >> $BashAuto
 fi
+echo "alias progress='progress -M'" >> $BashAuto
 
-#
-
-
+#Bat
+Bat=$(type -p bat)
+if [ ! "$Bat" ]; then
+    unzip -o $SoftDir'/bat.zip' -d $TmpDir
+    cp $TmpDir'/bat' /usr/local/bin/
+fi
 
 #导入自定义 bash 命令
 if [ $(cat ~/.bashrc | grep 'litsh' | wc -l) -eq 0 ] ; then
