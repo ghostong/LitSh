11c9084da7662a0b23812dce99b0da5a37bf742e
litong <ghostong@126.com>
2019-12-21 16:11:00 +0800

    修复语法不标准

--- init.sh
+++ init.sh
@@ -2,69 +2,69 @@
 
 #初始化变量
 WorkDir=$(cd `dirname $0`; pwd)
-SoftDir=$WorkDir'/software'
-TmpDir=$WorkDir'/tmp'
-BashAuto=$WorkDir'/litsh.auto.bashrc'
-BashOwn=$WorkDir'/litsh.bashrc'
+SoftDir=${WorkDir}'/software'
+TmpDir=${WorkDir}'/tmp'
+BashAuto=${WorkDir}'/litsh.auto.bashrc'
+BashOwn=${WorkDir}'/litsh.bashrc'
 
 #确认用户身份
-if [ ! `whoami` == 'root' ] ; then
+if [[ ! `whoami` == 'root' ]] ; then
     echo 'Operation not permitted, run this script as root'
     exit
 fi
 
-mkdir -p $TmpDir
+mkdir -p ${TmpDir}
 
 #确认 Package Manager
 PaMa=$(type -p 'apt-get' || type -p 'yum')
-if [ ! "$PaMa" ]; then
+if [[ ! "$PaMa" ]]; then
     echo "Your Package Manager ?"
     read PaMa
 fi
-echo "Use : "$PaMa
+echo "Use : "${PaMa}
 
 #初始化
-cd $WorkDir
-cat /dev/null > $BashAuto
+cd ${WorkDir}
+cat /dev/null > ${BashAuto}
 
 #软件源直接安装
-if [ "$PaMa" = "apt-get" ] ; then 
-    $PaMa update
+if [[ "$PaMa" = "apt-get" ]] ; then
+    ${PaMa} update
 else
-    $PaMa makecache
+    ${PaMa} makecache
 fi
-$PaMa install -y python-pip pkg-config
-$PaMa install -y htop iotop vim unzip tree screen nethogs
+${PaMa} install -y python-pip pkg-config
+${PaMa} install -y htop iotop vim unzip tree screen nethogs
 
 #pip 安装
 Speedometer=$(type -t speedometer)
-if [ ! "$Bat" ]; then
+if [[ ! "$Bat" ]]; then
     pip install speedometer
 fi
-echo "alias sm='speedometer'" >> $BashAuto
+echo "alias sm='speedometer'" >> ${BashAuto}
 
 #progress
 Progress=$(type -t progress)
-if [ ! "$Progress" ]; then
-    $PaMa install -y libncurses5-dev 
-    $PaMa install -y ncurses-devel
-    unzip -o $SoftDir'/progress.zip' -d $TmpDir
-    cd $TmpDir'/progress' && make && make install
+if [[ ! "$Progress" ]]; then
+    ${PaMa} install -y libncurses5-dev
+    ${PaMa} install -y ncurses-devel
+    unzip -o ${SoftDir}'/progress.zip' -d ${TmpDir}
+    cd ${TmpDir}'/progress' && make && make install
 fi
-echo "alias progress='/usr/local/bin/progress'" >> $BashAuto
+echo "alias progress='/usr/local/bin/progress'" >> ${BashAuto}
 
 #Bat
 Bat=$(type -t bat)
-if [ ! "$Bat" ]; then
-    unzip -o $SoftDir'/bat.zip' -d $TmpDir
-    cp $TmpDir'/bat' /usr/local/bin/
+if [[ ! "$Bat" ]]; then
+    unzip -o ${SoftDir}'/bat.zip' -d ${TmpDir}
+    cp ${TmpDir}'/bat' /usr/local/bin/
 fi
-echo "alias bat='/usr/local/bin/bat'" >> $BashAuto
+echo "alias bat='/usr/local/bin/bat'" >> ${BashAuto}
 
 #导入自定义 bash 命令
 BashrcFile=`ls /etc/* | grep bashrc | head -1`
-if [ $(cat $BashrcFile | grep 'litsh' | wc -l) -eq 0 ] ; then
-    echo "source $BashAuto" >> $BashrcFile
-    echo "source $BashOwn" >> $BashrcFile
+if [[ $(cat ${BashrcFile} | grep 'litsh' | wc -l) -eq 0 ]] ; then
+    echo "source $BashAuto" >> ${BashrcFile}
+    echo "source $BashOwn" >> ${BashrcFile}
 fi
 
